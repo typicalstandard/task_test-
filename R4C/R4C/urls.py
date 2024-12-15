af0dbf2110094ad9a8fba4d1f6de7940eef57dea
dikay <dikiy@gmai.com>
2024-12-16 00:04:02 +0300

    add urls скачать Excel

--- R4C/R4C/urls.py
+++ R4C/R4C/urls.py
@@ -1,21 +1,9 @@
-"""R4C URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 
+from R4C.robots.views import ExportRobotsView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('export_robots/', ExportRobotsView.as_view(), name='export_robots'),  # Новый маршрут
 ]
