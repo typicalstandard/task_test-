126a5c3581d1913f83d274a4d4268c5b2c87a80d
dikay <dikiy@gmai.com>
2024-12-15 23:55:00 +0300

    add urls для скачивания Excel

--- R4C/robots/urls.py
+++ R4C/robots/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from robots import CreateRobotAPIView
+from robots import CreateRobotAPIView,ExportRobotsView
 
 urlpatterns = [
     path('create_robot/', CreateRobotAPIView.as_view(), name='create_robot'),
+    path('export_robots/', ExportRobotsView.as_view(), name='export_robots'),
 ]
