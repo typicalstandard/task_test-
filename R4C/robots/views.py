30942197702e6458131304dc869b4e1de3a47f9d
dikay <dikiy@gmai.com>
2024-12-15 23:09:13 +0300

    add views для обработки post запроса в json формате

---
+++ R4C/robots/views.py
@@ -0,0 +1,24 @@
+from django.http import JsonResponse
+from django.views import View
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+import json
+
+from .RobotForm import RobotForm
+
+@method_decorator(csrf_exempt, name='dispatch')
+class CreateRobotAPIView(View):
+    def post(self, request, *args, **kwargs):
+        try:
+            data = json.loads(request.body)
+            form = RobotForm(data)
+            if form.is_valid():
+                form.save()
+                return JsonResponse({'message': 'Robot created successfully!'}, status=201)
+            else:
+                return JsonResponse({'errors': form.errors}, status=400)
+        except json.JSONDecodeError:
+            return JsonResponse({'error': 'Invalid JSON data'}, status=400)
+
+    def get(self, request, *args, **kwargs):
+        return JsonResponse({'error': 'Invalid HTTP method'}, status=405)
