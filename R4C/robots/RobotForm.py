2dacb6480dbe4cc65c2022b141867acbc24980d5
dikay <dikiy@gmai.com>
2024-12-18 01:29:49 +0300

    update добавил отдельный класс валидатора

--- R4C/robots/RobotForm.py
+++ R4C/robots/RobotForm.py
@@ -1,18 +1,36 @@
 from django import forms
-from .models import Robot
+from django.utils.deconstruct import deconstructible
 from django.utils.dateparse import parse_datetime
+from django.core.exceptions import ValidationError
+
+from .models import Robot
+
+@deconstructible
+class AlphanumericAndLengthValidator:
+    def __init__(self, max_length=2):
+        self.max_length = max_length
+
+    def __call__(self, value):
+        if not value:
+            raise ValidationError("Это поле не может быть пустым.")
+        if not value.isalnum() or len(value) > self.max_length:
+            raise ValidationError(f"Поле должно быть буквенно-цифровым и не более {self.max_length} символов.")
+        return value
+
 
 class RobotForm(forms.ModelForm):
     model = forms.CharField(
         label='Model',
         max_length=2,
-        required=True
+        required=True,
+        validators=[AlphanumericAndLengthValidator(max_length=2)]
     )
-    
+
     version = forms.CharField(
         label='Version',
         max_length=2,
-        required=True
+        required=True,
+        validators=[AlphanumericAndLengthValidator(max_length=2)]
     )
 
     created = forms.DateTimeField(
@@ -24,27 +42,11 @@ class RobotForm(forms.ModelForm):
         model = Robot
         fields = ['model', 'version', 'created']
 
-    def clean_model(self):
-        model = self.cleaned_data.get('model')
-        if not model:
-            raise forms.ValidationError("This field cannot be null.")
-        if not model.isalnum() or len(model) > 2:
-            raise forms.ValidationError("Model must be alphanumeric and no more than 2 characters.")
-        return model
-
-    def clean_version(self):
-        version = self.cleaned_data.get('version')
-        if not version:
-            raise forms.ValidationError("This field cannot be null.")
-        if not version.isalnum() or len(version) > 2:
-            raise forms.ValidationError("Version must be alphanumeric and no more than 2 characters.")
-        return version
-
     def clean_created(self):
         created = self.cleaned_data.get('created')
         if created:
             try:
                 parse_datetime(str(created))
             except ValueError:
-                raise forms.ValidationError("Enter a valid date and time.")
+                raise forms.ValidationError("Введите корректную дату и время.")
         return created
