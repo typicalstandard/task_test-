126c847e7767029a9cb6f8f4939dc0e7dd523bd5
dikay <dikiy@gmai.com>
2024-12-18 02:28:50 +0300

    update url для корректной работы

--- R4C/R4C/urls.py
+++ R4C/R4C/urls.py
@@ -3,6 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('robots/', include('robots.urls', namespace='robots')),
     path('orders/', include('orders.urls', namespace='orders')),
 ]
