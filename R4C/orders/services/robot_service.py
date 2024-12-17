a78e751a1bb56f4f74cccba316c91b261e523e9d
dikay <dikiy@gmai.com>
2024-12-18 00:39:48 +0300

    add проверка наличия робота

---
+++ R4C/orders/services/robot_service.py
@@ -0,0 +1,13 @@
+from robots.models import Robot
+from ..models import Order
+
+def check_robot_availability(model_name, version):
+    robots_count = Robot.objects.filter(model=model_name, version=version).count()
+    min_required_robots = 1
+
+    if robots_count >= min_required_robots:
+        return {"message": "Роботов достаточно в наличии."}, 200
+    else:
+        return {"message": "Недостаточное количество роботов в наличии."}, 404
+
+
