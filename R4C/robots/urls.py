b1451b2be3f00c96ac8f64111dbfd0a9a4057f11
typicalstandard <118108373+typicalstandard@users.noreply.github.com>
2024-12-18 00:50:37 +0300

    Update urls.py
    delete лишний код в таске

--- R4C/robots/urls.py
+++ R4C/robots/urls.py
@@ -1,10 +1,1 @@
-from django.urls import path
 
-from .views import CreateRobotAPIView,ExportRobotsView
-
-app_name = 'robots'
-
-urlpatterns = [
-    path('create_robot/', CreateRobotAPIView.as_view(), name='create_robot'),
-    path('export_robots/', ExportRobotsView.as_view(), name='export_robots'),
-]
