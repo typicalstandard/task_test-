263a67e083f9a299d14d6995811c504574df3d47
typicalstandard <118108373+typicalstandard@users.noreply.github.com>
2024-12-18 00:55:18 +0300

    Update views.py
    лишний код для таски

--- R4C/robots/views.py
+++ R4C/robots/views.py
@@ -1,30 +1,6 @@
-import json
-
-from django.http import JsonResponse, HttpResponse, HttpResponseServerError
-from django.utils.decorators import method_decorator
-from django.views.decorators.csrf import csrf_exempt
-from django.views import View
-
-from .RobotForm import RobotForm
 from .services.excel_export_service import ExcelExportService
-
-
-@method_decorator(csrf_exempt, name='dispatch')
-class CreateRobotAPIView(View):
-    def post(self, request, *args, **kwargs):
-        try:
-            data = json.loads(request.body)
-            form = RobotForm(data)
-            if form.is_valid():
-                form.save()
-                return JsonResponse({'message': 'Robot created successfully!'}, status=201)
-            else:
-                return JsonResponse({'errors': form.errors}, status=400)
-        except json.JSONDecodeError:
-            return JsonResponse({'error': 'Invalid JSON data'}, status=400)
-        except Exception as e:
-            return JsonResponse({'error': str(e)}, status=500)
-
+from django.http import  HttpResponse, HttpResponseServerError
+from django.views import View
 
 class ExportRobotsView(View):
     def get(self, request):
