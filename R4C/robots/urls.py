a249021a56f0c75796d8186c3d887f0787d68d8f
typicalstandard <118108373+typicalstandard@users.noreply.github.com>
2024-12-18 00:52:47 +0300

    Update urls.py
    лишний код для таски

--- R4C/robots/urls.py
+++ R4C/robots/urls.py
@@ -5,6 +5,5 @@ from .views import CreateRobotAPIView,ExportRobotsView
 app_name = 'robots'
 
 urlpatterns = [
-    path('create_robot/', CreateRobotAPIView.as_view(), name='create_robot'),
     path('export_robots/', ExportRobotsView.as_view(), name='export_robots'),
 ]
