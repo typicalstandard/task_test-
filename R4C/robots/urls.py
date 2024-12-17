337000777a3456dd690aee4ae7a36a9af350fc11
dikay <dikiy@gmai.com>
2024-12-18 02:02:11 +0300

    update лишний код для таски

--- R4C/robots/urls.py
+++ R4C/robots/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 
-from .views import CreateRobotAPIView,ExportRobotsView
+from .views import CreateRobotAPIView
 
 app_name = 'robots'
 
 urlpatterns = [
     path('create_robot/', CreateRobotAPIView.as_view(), name='create_robot'),
-    path('export_robots/', ExportRobotsView.as_view(), name='export_robots'),
 ]
