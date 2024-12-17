170657984ed722e050f96d43a8bd1d10de3b231e
dikay <dikiy@gmai.com>
2024-12-18 00:38:54 +0300

    create вьюха для обработки заказа

--- R4C/orders/views.py
+++ R4C/orders/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.http import JsonResponse, HttpResponse
+from django.views import View
+from customers.models import Customer
+from .services.robot_service import check_robot_availability, create_order
 
-# Create your views here.
+class NotifyCustomerView(View):
+    def get(self, request, model_name, version, customer_id):
+        try:
+            customer = Customer.objects.get(pk=customer_id)
+
+            response, status = check_robot_availability(model_name, version)
+            if status == 200:
+                return HttpResponse(response["message"], status=status)
+
+            create_order(customer, model_name, version)
+            return HttpResponse("Недостаточное количество роботов в наличии", status=404)
+        except Customer.DoesNotExist:
+            return HttpResponse("Клиент не найден", status=404)
+        except Exception as e:
+            return HttpResponse(str(e), status=500)
