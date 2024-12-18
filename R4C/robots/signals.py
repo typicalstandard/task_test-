41bb9ff07f15def4cb2f5c52f55b7ecac7985ce8
dikay <dikiy@gmai.com>
2024-12-18 22:36:11 +0300

    update улучшим сигналы разделение логики по сервисам

--- R4C/robots/signals.py
+++ R4C/robots/signals.py
@@ -1,28 +1,12 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django.core.mail import send_mail
 from .models import Robot
-from orders.models import Order
+from orders.services.notification_services import notify_customers
+from .services.robot_services import set_robot_serial
+
 
 @receiver(post_save, sender=Robot)
-def notify_customers(sender, instance, **kwargs):
-    if instance.pk is None:
-        return
-
-    orders = Order.objects.filter(robot_serial=instance.serial, status=Order.Status.WAITING)
-    customer = orders.customer
-    try:
-        send_mail(
-            'Робот в наличии',
-            f'Добрый день!\n\n'
-            f'Недавно вы интересовались нашим роботом модели {instance.model}, версии {instance.version}.\n'
-            f'Этот робот теперь в наличии. Если вам подходит этот вариант - пожалуйста, свяжитесь с нами.',
-            None,
-            [customer.email],
-            fail_silently=False,
-        )
-        orders.status = Order.Status.READY
-        orders.save()
-        print(f"Письмо успешно отправлено клиенту: {customer.email}")
-    except Exception as e:
-        print(f"Ошибка при отправке письма клиенту {customer.email}: {e}")
+def handle_robot_save(sender, instance, created, **kwargs):
+    if created:
+        set_robot_serial(instance)
+    notify_customers(instance)
